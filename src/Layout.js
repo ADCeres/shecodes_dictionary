fc1e0b87f8fa56c7e3da7d5ac56500031a63fd44
ADCeres <82452034+ADCeres@users.noreply.github.com>
2022-02-21 15:14:17 -0700

    Footer Added

--- src/Layout.js
+++ src/Layout.js
@@ -7,6 +7,14 @@ function Layout() {
     <div>
       <Header />
       <Search />
+      <div className="footer">
+        Coded by Alyson Felton
+        <br />
+        <a href="https://github.com/ADCeres/shecodes_dictionary">
+          https://github.com/ADCeres/shecodes_dictionary
+        </a>
+        <br />
+      </div>{" "}
     </div>
   );
 }
