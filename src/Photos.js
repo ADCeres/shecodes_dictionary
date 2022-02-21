e76c587f9baf9c60e882c3a708092df51e9de00d
ADCeres <82452034+ADCeres@users.noreply.github.com>
2022-02-21 16:08:18 -0700

    Img Alt Error Fix

--- src/Photos.js
+++ src/Photos.js
@@ -14,6 +14,7 @@ function Photos(props) {
                     <img
                       src={photo.src.landscape}
                       className="img-fluid word-photo"
+                      alt="pic"
                     />
                   </a>
                 </div>
